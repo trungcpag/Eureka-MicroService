5643c2ab06a11a47c8717a8ec07b85170740bc7e
trungcpag <trungcpag@gmail.com>
2021-10-12 20:52:01 +0700

    add config server

---
+++ PhotoAppApiConfigServer/src/test/java/com/app/photoappapiconfigserver/PhotoAppApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.photoappapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
